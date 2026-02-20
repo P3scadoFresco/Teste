2d73b834705d0c15b2054592eafe3889aa064bde
P3scadoFresco <rafoso039@gmail.com>
2026-02-20 15:58:36 -0300

    verção final do exercicio da fila circular

--- EstruturaCircular.java
+++ EstruturaCircular.java
@@ -2,27 +2,37 @@ import java.util.Scanner;
 
 public class EstruturaCircular {
 
-    public static int removeMaiorQueDez(int vetor[]){
+    public static int[] removeMaiorQueDez(int vetor[]){
 
         int tamanho = vetor.length;
-        int resultado = 0;
+        int resultado = -1;
 
         for(int i = 0; i < tamanho; i++){
-            if (vetor[i] == 10);
+            if (vetor[i] > 10){
                 resultado = vetor[i];
+            
+                for(int j = i; j < tamanho - 1; j++){
+                    vetor[j] = vetor[j+1];
+                }
+                vetor[tamanho - 1] = 0;
+                System.out.println("valor removido: " + resultado);
+                return vetor;
             }
+        }
 
-            return resultado;
+        System.out.println("valor nao encontrado");
+        return vetor;
     }
 
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
 
-        int vetor[] = scanner.nextInt();
+        int tamanho = scanner.nextInt();
+        int[] vetor = new int[tamanho];
 
-        for(int i = 0; i != '\0'; i++){
-            vetor[i] = i;
+        for(int i = 0; i < tamanho; i++){
+            vetor[i] = scanner.nextInt();
         }
 
         scanner.close();
